2b809bbf5de4a5fe2fc0e4c98c1022ccb36cca82
beenomjee <125212257+beenomjee@users.noreply.github.com>
2023-04-06 23:17:17 +0500

    update biolerplate folder

--- biolerplate/src/App.jsx
+++ biolerplate/src/App.jsx
@@ -1,6 +1,6 @@
 import { Home, Contact } from './modules'
 import { Button, Footer, Header } from './components'
-import { Logo } from './assets'
+import { Logo, Mic, Rating } from './assets'
 import { store } from './store'
 
 const App = () => {
@@ -12,6 +12,8 @@ const App = () => {
       <Footer></Footer>
       <Header></Header>
       <img src={Logo} alt="####" />
+      <img src={Rating} alt="####" />
+      <img src={Mic} alt="####" />
       <h1>{store}</h1>
     </div>
   )
--- biolerplate/src/assets/index.jsx
+++ biolerplate/src/assets/index.jsx
@@ -1,1 +1,3 @@
 export { default as Logo } from './images/react.svg';
+export { default as Rating } from './images/rating.svg';
+export { default as Mic } from './images/mic.svg';
